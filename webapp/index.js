83390a4785bc3e2a97facd6cdf359e22682a998f
Nevskiy Vladislav <VNevskii650@gmail.com>
2022-07-25 12:36:06 +0200

    view and controller

--- webapp/index.js
+++ webapp/index.js
@@ -2,7 +2,7 @@ sap.ui.define(["sap/ui/core/mvc/XMLView"], function (XMLView) {
   "use strict";
 
   XMLView.create({
-    viewName: "sapwalkthrough.webapp.view.App",
+    viewName: "sap.ui.demo.walkthrough.view.App",
   }).then(function (oView) {
     oView.placeAt("content");
   });
