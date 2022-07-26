8c4a202cf5530c73e7be8cc94bb8523602c928b6
Nevskiy Vladislav <VNevskii650@gmail.com>
2022-07-26 10:48:34 +0200

    x`

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,6 +1,10 @@
 sap.ui.define(
-  ["sap/ui/core/UIComponent", "sap/ui/model/json/JSONModel"],
-  function (UIComponent, JSONModel) {
+  [
+    "sap/ui/core/UIComponent",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel",
+  ],
+  function (UIComponent, JSONModel, ResourceModel) {
     "use strict";
     return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
       metadata: {
@@ -11,13 +15,20 @@ sap.ui.define(
         // call the init function of the parent
         UIComponent.prototype.init.apply(this, arguments);
         // set data model
-        var oData = {
+        let oData = {
           recipient: {
-            name: "World",
+            name: "Владислав",
+            surname: "Невский",
           },
         };
-        var oModel = new JSONModel(oData);
+
+        const oModel = new JSONModel(oData);
         this.setModel(oModel);
+        // set i18n model on view
+        const i18nModel = new ResourceModel({
+          bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
+        });
+        this.setModel(i18nModel, "i18n");
       },
     });
   }
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -8,27 +8,13 @@ sap.ui.define(
   function (Controller, MessageToast, JSONModel, ResourceModel) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-      onInit: function () {
-        // set data model on view
-        let oData = {
-          recipient: {
-            name: "Владислав",
-            surname: "Невский",
-          },
-        };
-        const oModel = new JSONModel(oData);
-        this.getView().setModel(oModel);
-        // set i18n model on view
-        var i18nModel = new ResourceModel({
-          bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
-        });
-        this.getView().setModel(i18nModel, "i18n");
-      },
       onShowHello: function (oEvent) {
         // read msg from i18n model
         var oBundle = this.getView().getModel("i18n").getResourceBundle();
         var sName = this.getView().getModel().getProperty("/recipient/name");
-        var sSurname = this.getView().getModel().getProperty("/recipient/name");
+        var sSurname = this.getView()
+          .getModel()
+          .getProperty("/recipient/surname");
         var sMsg = oBundle.getText("helloMsg", [sName, sSurname]);
         // show message
         MessageToast.show(sMsg);
