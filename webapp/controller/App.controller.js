72cd3a232727945206a8f1fbbbefb8d4a82b540a
Nevskiy Vladislav <VNevskii650@gmail.com>
2022-07-26 13:24:34 +0200

    15

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,24 +1,4 @@
-sap.ui.define(
-  [
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel",
-  ],
-  function (Controller, MessageToast, JSONModel, ResourceModel) {
-    "use strict";
-    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-      onShowHello: function (oEvent) {
-        // read msg from i18n model
-        var oBundle = this.getView().getModel("i18n").getResourceBundle();
-        var sName = this.getView().getModel().getProperty("/recipient/name");
-        var sSurname = this.getView()
-          .getModel()
-          .getProperty("/recipient/surname");
-        var sMsg = oBundle.getText("helloMsg", [sName, sSurname]);
-        // show message
-        MessageToast.show(sMsg);
-      },
-    });
-  }
-);
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
+  "use strict";
+  return Controller.extend("sap.ui.demo.walkthrough.controller.App", {});
+});
